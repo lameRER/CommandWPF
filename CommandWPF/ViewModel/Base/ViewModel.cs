4aaf7c19ce648e7745663dcf36a92d4aef7fde89
lameR <erpresents@gmail.com>
2022-04-16 00:19:57 +0300

    Добавлена реализация интерфейса INotifyPropertyChanging

---
+++ CommandWPF/ViewModel/Base/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandWPF.ViewModel.Base
+{
+    internal class ViewModel : INotifyPropertyChanging
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        protected virtual void OnPropertyChanging([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T fiend, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(fiend, value)) return false;
+            fiend = value;
+            OnPropertyChanging();
+            return true;
+        }
+    }
+}
