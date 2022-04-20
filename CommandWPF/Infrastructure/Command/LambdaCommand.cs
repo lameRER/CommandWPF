1bc2765e289c909124b0d36cc86f7de3f84972ca
lameR <erpresents@gmail.com>
2022-04-20 18:19:08 +0300

    Добавлена ревализация интерфейса ICommand

---
+++ CommandWPF/Infrastructure/Command/LambdaCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CommandWPF.Infrastructure.Command
+{
+    internal class LambdaCommand : Base.Command
+    {
+        private readonly Action<object?> _onExecute;
+        private readonly Func<object?, bool>? _onCanExecute;
+
+        public LambdaCommand(Action<object?> onExecute, Func<object?, bool>? onCanExecute = null)
+        {
+            _onExecute = onExecute;
+            _onCanExecute = onCanExecute;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _onExecute(parameter);
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return base.CanExecute(parameter) && (_onCanExecute?.Invoke(parameter) ?? true);
+        }
+    }
+}
